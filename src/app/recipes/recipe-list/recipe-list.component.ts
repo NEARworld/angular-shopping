d3cb1ec5d36ef2a259815eb6f1ccd4fd89b31bfd
nearworld <artemismars2@outlook.com>
2023-12-26 19:32:47 +0900

    feat: recipe list markup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,6 +13,11 @@ export class RecipeListComponent {
       'This is simply a test',
       'https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-03/plant-based-food-mc-220323-02-273c7b.jpg'
     ),
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2022-03/plant-based-food-mc-220323-02-273c7b.jpg'
+    ),
   ];
 
   constructor() {}
